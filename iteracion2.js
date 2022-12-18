6f1103de2308667539bd6511eba209391d7c311e
victorgarciaz <victor.garcia@bootcamp-upgrade.com>
2022-12-18 18:56:42 +0100

    iteracion1.js

--- iteracion2.js
+++ iteracion2.js
@@ -8,6 +8,7 @@ console.log(character);
 let firstName = 'Jon';
 let lastName = 'Snow';
 let age = 24;
+
 /*Muestralos por consola de esta forma: 
 	'Soy Jon Snow, tengo 24 años y me gustan los lobos.'*/
 console.log("Soy " + firstName + " " + lastName + "," + age "años y me gustan los lobos");
