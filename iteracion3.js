32f521f2b77ea2ebff836d50b1ea28902110d873
victorgarciaz <victor.garcia@bootcamp-upgrade.com>
2022-12-18 18:19:48 +0100

    upgrade-basic-algorithms

---
+++ iteracion3.js
@@ -0,0 +1,28 @@
+/*1.1 Multiplica 10 por 5 y muestra el resultado mediante console.log().*/
+let mult = 10 * 5
+console.log (mult);
+
+/*1.2 Divide 10 por 2 y muestra el resultado en un console.log().*/
+let div = 10 / 2;
+console.log(div);
+
+/*1.3 Muestra mediante un console.log() el resto de dividir 15 por 9.*/
+let rest = 10 % 9;
+console.log(rest);
+
+/*1.4 Usa el correcto operador de asignación que resultará en x = 15, 
+teniendo dos variables y = 10 y z = 5.*/
+
+let x = y +z;
+y = 10;
+z = 5;
+
+console.log(x);
+
+/*1.5 Usa el correcto operador de asignación que resultará en x = 50,
+teniendo dos variables y = 10 y z = 5.*/
+let x = y *z
+y = 10;
+z = 5;
+
+console.log(x);
